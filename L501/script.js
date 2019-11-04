6d6068fcc2954bb6f2c71a1e07fc677fc1cb0f93
davit seiranovi <reedevild@gmail.com>
2019-11-04 14:27:13 +0400

    s

---
+++ L501/script.js
@@ -0,0 +1,6 @@
+let x = document.querySelector("head");
+let u = document.body.querySelector("ul")
+let u2 = document.body.querySelector("#u");
+document.body.querySelector("#u").style.backgroundColor = "red";
+
+
---
+++ L502/script.js
@@ -0,0 +1,12 @@
+//if (('#idDiv').is(':empty')){
+  //  alert("element is empty :D");
+  //}
+  //else{
+   //   alert("its fully")
+  //}
+/*var x = document.body.querySelector("#idDiv");
+if (x.isEmp {
+    alert("carielia")
+    
+}
+*/
